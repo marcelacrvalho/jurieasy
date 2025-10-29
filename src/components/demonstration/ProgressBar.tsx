7bf12b2c05e864476cdd6499dc96e4c9c1577bc9
Marcela <marcelacrv@icloud.com>
2025-10-29 19:18:40 -0300

    FEATURE: Altera cor da progressbar

--- src/components/demonstration/ProgressBar.tsx
+++ src/components/demonstration/ProgressBar.tsx
@@ -6,7 +6,7 @@ export default function ProgressBar({ progress }: ProgressBarProps) {
     return (
         <div className="w-full bg-gray-200 rounded-full h-2 mt-3">
             <div
-                className="bg-blue-600 h-2 rounded-full transition-all duration-500 ease-out"
+                className="bg-green-600 h-2 rounded-full transition-all duration-500 ease-out"
                 style={{ width: `${progress}%` }}
             />
         </div>
